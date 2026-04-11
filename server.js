533609fabaa1ea944c3d9812979a386bf6208fb3
revathiganeshinfo-cpu <revathiganeshinfo@gmail.com>
2026-04-11 12:15:11 +0530

    fix cors

--- server.js
+++ server.js
@@ -19,7 +19,10 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://fsd-project-frontend.vercel.app",
+  credentials: true
+}));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
